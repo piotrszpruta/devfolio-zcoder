ae6da518f468e849b246b373a2233feb03376c32
Piotr Szpruta <kontakt@zcoderx.com>
2023-04-15 22:21:47 +0000

    Adapt project section to show blogs

--- src/components/section-projects/index.jsx
+++ src/components/section-projects/index.jsx
@@ -4,16 +4,15 @@ import Section from '../section';
 import SummaryItem from '../summary-item';
 
 const SectionProjects = ({ projects }) => {
-  if (!projects.length) return null;
-
   return (
     <Section title="Projects">
       {projects.map((project) => (
         <SummaryItem
-          key={project.name}
-          name={project.name}
-          description={project.description}
-          link={project.link}
+          key={project.node.fields.slug}
+          name={project.node.frontmatter.title}
+          description={project.node.frontmatter.description}
+          link={project.node.fields.slug}
+          internal
         />
       ))}
     </Section>
