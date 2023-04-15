12999796ac71ea16f420a11734d4d95f3e7abf08
Piotr Szpruta <kontakt@zcoderx.com>
2023-04-15 22:23:56 +0000

    Remove unused code and adapt to personal preferences

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,8 +4,6 @@ import React from 'react';
 
 import Header from '../components/header';
 import Layout from '../components/layout';
-import SectionAbout from '../components/section-about';
-import SectionBlog from '../components/section-blog';
 import SectionExperience from '../components/section-experience';
 import SectionProjects from '../components/section-projects';
 import SectionSkills from '../components/section-skills';
@@ -13,19 +11,16 @@ import SEO from '../components/seo';
 
 const Index = ({ data }) => {
   const about = get(data, 'site.siteMetadata.about', false);
-  const projects = get(data, 'site.siteMetadata.projects', false);
-  const posts = data.allMarkdownRemark.edges;
+  const projects = data.allMarkdownRemark.edges;
   const experience = get(data, 'site.siteMetadata.experience', false);
   const skills = get(data, 'site.siteMetadata.skills', false);
-  const noBlog = !posts || !posts.length;
+  const noProjects = !projects || !projects.length;
 
   return (
     <Layout>
       <SEO />
-      <Header metadata={data.site.siteMetadata} noBlog={noBlog} />
-      {about && <SectionAbout about={about} />}
-      {projects && projects.length && <SectionProjects projects={projects} />}
-      {!noBlog && <SectionBlog posts={posts} />}
+      <Header metadata={data.site.siteMetadata} />
+      {!noProjects && <SectionProjects projects={projects} />}
       {experience && experience.length && (
         <SectionExperience experience={experience} />
       )}
