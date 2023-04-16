12702a312567ffc9e1be3d5c2b34702fe24dcde5
Piotr Szpruta <kontakt@zcoderx.com>
2023-04-16 17:36:45 +0000

    Add icons before active links and change color to green on hover

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -3,23 +3,33 @@ import React from 'react';
 
 const classes = {
   wrapper: 'mb-6',
-  name: 'font-semibold text-gray-900 pb-1',
+  name: 'font-semibold text-gray-900 pb-1 ',
   description: 'text-md text-gray-600 font-light',
 };
 
 const SummaryItem = ({ name, description, link = false, internal = false }) => {
   let linkContent;
   if (internal) {
-    linkContent = <Link to={link}>{name}</Link>;
+    linkContent = <Link to={link}>
+        {name}
+        <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg>
+      </Link>;
   } else {
-    linkContent = <a href={link}>{name}</a>;
+    linkContent = <a href={link}>
+        {name}
+        <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg>
+      </a>;
   }
 
   return (
     <div className={classes.wrapper}>
       <h3
         className={`${classes.name} ${
-          link ? 'hover:underline hover:text-black' : ''
+          link ? 'hover:underline hover:text-green-500' : ''
         }`}
       >
         {link ? linkContent : name}
