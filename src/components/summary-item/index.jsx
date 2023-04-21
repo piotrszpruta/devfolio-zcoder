41d5d8dc7a39f3f132b47a2a8e61d00a3f2d3f52
Piotr Szpruta <kontakt@zcoderx.com>
2023-04-21 16:21:54 +0000

    Format file

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -11,26 +11,25 @@ const SummaryItem = ({ name, description, link = false, internal = false }) => {
   let linkContent;
   if (internal) {
     linkContent = <Link to={link}>
-        {name}
-        <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-        </svg>
-      </Link>;
+      {name}
+      <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+      </svg>
+    </Link>;
   } else {
     linkContent = <a href={link}>
-        {name}
-        <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-        </svg>
-      </a>;
+      {name}
+      <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+      </svg>
+    </a>;
   }
 
   return (
     <div className={classes.wrapper}>
       <h3
-        className={`${classes.name} ${
-          link ? 'hover:underline hover:text-green-500' : ''
-        }`}
+        className={`${classes.name} ${link ? 'hover:underline hover:text-green-500' : ''
+          }`}
       >
         {link ? linkContent : name}
       </h3>
