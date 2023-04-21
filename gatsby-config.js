7ffca5eddffe119db649c41379178cd1c1a25128
Piotr Szpruta <kontakt@zcoderx.com>
2023-04-21 21:09:02 +0000

    Add lazy loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,6 +88,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-lazy-load`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
